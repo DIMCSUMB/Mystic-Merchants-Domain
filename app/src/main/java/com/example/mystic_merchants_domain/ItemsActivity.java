61e574e17a24edaf00c1903d218b4c051bcf6611
Daniel I. Martinez <danielmartinez2@csumb.edu>
2023-11-29 15:15:17 -0800

    Created an items list where the database adds all the items at the start of the app
    Whenever I try to go to Items page it crashes. Trying to figure out problems

--- app/src/main/java/com/example/mystic_merchants_domain/ItemsActivity.java
+++ app/src/main/java/com/example/mystic_merchants_domain/ItemsActivity.java
@@ -1,13 +1,33 @@
 package com.example.mystic_merchants_domain;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
+
 import android.os.Bundle;
 
+import java.util.List;
+
 public class ItemsActivity extends AppCompatActivity {
+
+    private AppDatabase database;
+    private List<Items> itemsList;
+    private RecyclerView recyclerView;
+    private ItemsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
+
+        database = Room.databaseBuilder(getApplicationContext(), AppDatabase.class,
+                "merchants_database").build();
+        itemsList = database.itemsDAO().getAllItems();
+
+        recyclerView = findViewById(R.id.recycler_view);
+        adapter = new ItemsAdapter(this, itemsList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
